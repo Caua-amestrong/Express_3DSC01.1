3c3b15d1004a91c45e661f47f7ed947596378d5a
caua <130366429+Caua-amestrong@users.noreply.github.com>
2024-10-03 11:36:40 +0000

    index atualizado

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ const app = express();
 const PORT = 3300;
 
 app.get("/", function (req, res) {
-  //res.sendFile()
-  res.send(__dirname);
+  res.sendFile(__dirname + "/src/views/index.html");
+  //res.send(__dirname);
 });
+app.get("/sobre", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+
+app.get("/404", function (req, res) {
+  res.sendFile(__dirname + "/src/views/404.html");
+});
+
 app.listen(PORT, () => {
   console.log("running...");
 });
